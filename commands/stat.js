4d8a60df4171648e2427eb804d40209f0a02a3dd
Jeppevinkel <jeppevinkel@gmail.com>
2020-08-21 20:17:16 +0200

    Eighth commit

--- commands/stat.js
+++ commands/stat.js
@@ -1,12 +1,34 @@
+const Discord = require("discord.js");
+
 exports.run = (client, message, args, level) => {
   if (!message.mentions.members.size) {
+    if (!client.stats.get(message.member.id)) {
+      client.stats.set(message.member.id, 0);
+    }
+
     let score = client.stats.get(message.member.id);
 
-    message.channel.send("You have a score of: " + score, {code: "asciidoc", split: { char: "\u200b" }});
+    let embed = new Discord.MessageEmbed()
+      .setColor('#0099ff')
+      .setAuthor(message.member.displayName, message.member.user.displayAvatarURL())
+      .addField('Love', score)
+      .setTimestamp();
+
+    message.channel.send(embed);
   } else {
+    if (!client.stats.get(message.mentions.members.first().id)) {
+      client.stats.set(message.mentions.members.first().id, 0);
+    }
+
     let score = client.stats.get(message.mentions.members.first().id);
 
-    message.channel.send("You have a score of: " + score, {code: "asciidoc", split: { char: "\u200b" }});
+    let embed = new Discord.MessageEmbed()
+      .setColor('#0099ff')
+      .setAuthor(message.mentions.members.first().displayName, message.mentions.members.first().user.displayAvatarURL())
+      .addField('Love', score)
+      .setTimestamp();
+
+    message.channel.send(embed);
   }
 };
 
